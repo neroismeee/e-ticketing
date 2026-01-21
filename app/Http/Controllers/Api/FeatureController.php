46a202b5cf38e2a75052798970440cb8e6732d93
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-21 09:11:05 +0800

    feat: create FeatureController

---
+++ app/Http/Controllers/Api/FeatureController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\FeatureRequest;
+use Illuminate\Http\Request;
+
+class FeatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = FeatureRequest::orderBy('created_at', 'desc')->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'List of Feature Requests',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

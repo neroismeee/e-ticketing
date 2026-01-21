60a1f906d1fc80186fc177b77771d95cc472df30
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-21 09:12:22 +0800

    feat: create api.php

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TicketController;
+use App\Http\Controllers\Api\ErrorController;
+use App\Http\Controllers\Api\FeatureController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+//ticket routes
+Route::get('/tickets', [TicketController::class, 'index']);
+Route::get('/tickets/{id}', [TicketController::class, 'show']);
+
+//error report routes
+Route::get('/error-reports', [ErrorController::class, 'index']);
+
+//feature request routes
+Route::get('/feature-requests', [FeatureController::class, 'index']);

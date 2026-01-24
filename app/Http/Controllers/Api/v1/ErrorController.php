02643bccf8e92eab187c54f80fe3f898b8742285
Aditya Suryandaru <suryandarua@gmail.com>
2026-01-24 10:19:37 +0800

    feat: create API versioning

---
+++ app/Http/Controllers/Api/v1/ErrorController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\ErrorReport;
+use Illuminate\Http\Request;
+use App\Http\Requests\ErrorRequest;
+use App\Http\Resources\ErrorResource;
+
+class ErrorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = ErrorResource::collection(ErrorReport::latest()->get());
+        return response()->json([
+            'status' => true,
+            'message' => 'List of Error Reports',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ErrorRequest $request)
+    {
+        $data = $request->validated();
+
+        $errorReport = ErrorReport::create($data);
+        return response()->json([
+            'status' => true,
+            'message' => 'Error Report Created Successfully',
+            'data' => $errorReport
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = ErrorReport::findorFail($id);
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = ErrorReport::findOrFail($id);
+        $data->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Error Report Updated Successfully',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $data = ErrorReport::findOrFail($id);
+        $data->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Error Report Deleted Successfully',
+        ], 200);
+    }
+}
---
+++ app/Http/Controllers/Api/v1/FeatureController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\FeatureRequest as RequestsFeatureRequest;
+use App\Http\Resources\FeatureResource;
+use App\Models\FeatureRequest;
+use Illuminate\Http\Request;
+
+class FeatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = FeatureResource::collection(FeatureRequest::orderBy('created_at', 'desc')->get());
+        return response()->json([
+            'status' => true,
+            'message' => 'List of Feature Requests',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(RequestsFeatureRequest $request)
+    {
+        $data = $request->validated();
+
+        $featureRequest = FeatureRequest::create($data);    
+        return response()->json([
+            'status' => true,
+            'message' => 'Feature Request Created Successfully',
+            'data' => $featureRequest
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = FeatureRequest::findOrFail($id);
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = FeatureRequest::findOrFail($id);
+        $data->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Feature Request Updated Successfully',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $data = FeatureRequest::findOrFail($id);
+        $data->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Feature Request Deleted Successfully',
+        ], 200);
+    }
+}
